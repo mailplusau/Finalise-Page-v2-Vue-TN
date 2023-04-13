04225ed44f7025028c03b12c71f35afc821b5d42
Thien Nguyen <s3426515@rmit.edu.vn>
2023-04-13 23:46:53 +1000

    Add Vuex modules and Global Modal

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,74 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import modules from './modules';
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+const state = {
+    globalModal: {
+        open: false,
+        title: '',
+        body: '',
+        busy: false,
+        persistent: true,
+        isError: false
+    },
+    industries: [],
+    leadSources: [],
+    franchisees: [],
+    roles: [],
+    statuses: [
+        {value: 6, text: 'SUSPECT - New'},
+        {value: 57, text: 'SUSPECT - Hot Lead'},
+        {value: 13, text: 'CUSTOMER - Signed'},
+    ],
+    states: [
+        {value: 1, text: 'NSW'},
+        {value: 2, text: 'QLD'},
+        {value: 3, text: 'VIC'},
+        {value: 4, text: 'SA'},
+        {value: 5, text: 'TAS'},
+        {value: 6, text: 'ACT'},
+        {value: 7, text: 'WA'},
+        {value: 8, text: 'NT'},
+        {value: 9, text: 'NZ'},
+    ],
+};
+
+const getters = {
+    industries : state => state.industries,
+    leadSources : state => state.leadSources,
+    franchisees : state => state.franchisees.filter(item => item.text.toLowerCase().substring(0, 4) !== 'old '), // filter out franchisees with name starting with 'old'
+    roles : state => state.roles,
+    statuses : state => state.statuses,
+    states : state => state.states,
+    globalModal : state => state.globalModal,
+};
+
+const mutations = {
+    setGlobalModal :  (state, open = true) => { state.globalModal.open = open; },
+    displayErrorGlobalModal : (state, {title, message}) => {
+        state.globalModal.title = title;
+        state.globalModal.body = message;
+        state.globalModal.busy = false;
+        state.globalModal.open = true;
+        state.globalModal.persistent = true;
+        state.globalModal.isError = true;
+    }
+};
+
+const actions = {
+    init : () => {
+        document.querySelector('h1.uir-record-type').setHTML('');
+    }
+};
+
+const store = new Vuex.Store({
+  state,
+  mutations,
+  actions,
+  getters,
+  modules,
+});
+
+export default store;
